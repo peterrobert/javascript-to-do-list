efe19edeb8ff042d673d460958ae4fdfb0d895f9
peterrobert <pwambui93@gmail.com>
2020-08-25 22:13:40 +0300

    created and saved items to local storage

--- src/logic.js
+++ src/logic.js
@@ -2,41 +2,42 @@ import {
     PageDisplay
 } from "./display.js";
 
-
 class Logic {
-
-
-
     static projectSubmit() {
+        let Store = [];
 
+        PageDisplay.DomManupilation().btn_container.addEventListener(
+            "click",
+            () => {
+                const projectValue = PageDisplay.DomManupilation().ProjectTitleVal;
 
-        let Storage = [];
+                if (projectValue.value == "") {
+                    console.log("its empty");
+                } else {
+                    Store.push(projectValue.value);
+                }
 
-        PageDisplay.DomManupilation().btn_container.addEventListener("click", () => {
-            const projectValue = PageDisplay.DomManupilation().ProjectTitleVal;
-
-            if (projectValue.value == '') {
-
-                console.log('its empty')
-
-            } else {
-             
-                Storage.push(projectValue.value);
-
+                StorageContainer();
             }
+        );
 
+        let StorageContainer = () => {
+            // Check browser support
+            if (typeof (Storage) !== "undefined") {
+                // Store
+                localStorage.setItem("container", `${ Store }`);
+                // Retrieve
+              console.log(Store)
+            } else {
+              console.log('this hasnt worked');
+            }
+        }
 
-            console.log(Storage)
-
-        });
-
-        return{Storage}
-
+       
     };
 
 }
 
-
 export {
     Logic
-}
+};
